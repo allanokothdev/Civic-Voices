6681f190b8a54610d42300177e60aacb96daa135
Allan Okoth <allanokoth695@gmail.com>
2023-12-31 10:26:44 +0200

    update 50 files

--- fragments/Feed.js
+++ fragments/Feed.js
@@ -25,7 +25,7 @@ const Feed = () => {
     return (
         <div className='sm:ml-[81px] xl:ml-[340px] w-[600px] min-h-screen border-r border-gray-400 text-white py-2'>
             
-            <div className='bg-[#16181C] flex gap-2 rounded-full py-2 px-4 text-white items-center text-[20px] sticky top-1 z-10'>
+            <div className='bg-[#16181C] flex gap-2 rounded-full py-2 px-4 text-white items-cennter text-[20px] sticky top-1 z-10'>
                 <FiSearch />
                 <input className='bg-transparent w-[100%] outline-none' type="text" placeholder='Search Twitter' />
             </div>
